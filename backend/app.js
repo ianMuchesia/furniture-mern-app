69e4d1c73ebbe2f83a3fe3716f9b93228da9b51c
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-18 11:42:33 +0300

    start backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
-console.log("here")
+const express = require('express')
+const fileUpload = require('express-fileupload')
+var cloudinary = require('cloudinary').v2;
+const products = require('./products')
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).json({msg:products})
+})
+
+const port = 3000
+
+
+app.listen(port, ()=>{
+    console.log(`${products.length}`)
+})
